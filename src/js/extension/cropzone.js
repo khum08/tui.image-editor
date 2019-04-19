88f41de5f6aa5df90cee62e3f06eacecae16e26e
junghwan-park <junghwan.park@nhnent.com>
2019-04-19 17:27:16 +0900

    fix: cropzone outerRect

--- src/js/extension/cropzone.js
+++ src/js/extension/cropzone.js
@@ -40,22 +40,22 @@ const Cropzone = fabric.util.createClass(fabric.Rect, /** @lends Cropzone.protot
         this.canvas = canvas;
         this.options = options;
 
-        this.on({
-            'moving': this._onMoving,
-            'scaling': this._onScaling
+        this.canvas.on({
+            'object:moving': this._onMoving.bind(this),
+            'object:scaling': this._onScaling.bind(this)
         });
     },
 
     /**
      * Render Crop-zone
-     * @param {CanvasRenderingContext2D} ctx - Context
+     * @param {CanvasRenderingContext2D} tempCtx - Context
      * @private
      * @override
      */
-    _render(ctx) {
+    _render(tempCtx) {
         const cropzoneDashLineWidth = 7;
         const cropzoneDashLineOffset = 7;
-        this.callSuper('_render', ctx);
+        this.callSuper('_render', tempCtx);
 
         // Calc original scale
         const originalFlipX = this.flipX ? -1 : 1;
@@ -64,6 +64,7 @@ const Cropzone = fabric.util.createClass(fabric.Rect, /** @lends Cropzone.protot
         const originalScaleY = originalFlipY / this.scaleY;
 
         // Set original scale
+        const ctx = this.canvas.getContext();
         ctx.scale(originalScaleX, originalScaleY);
 
         // Render outer rect
